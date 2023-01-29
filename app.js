9160704fe663e019f6eab868633c1c63ae00567f
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-01-29 21:04:04 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,156 @@
+require('dotenv').config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found.js")
+
+// MIDDLEWARE
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'This is a secret key',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.MONGO_URI);
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// GET REQUESTS
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.get("/register", (req, res) => {
+    res.render("register")
+});
+
+app.get("/login", (req, res) => {
+    res.render("login")
+});
+
+app.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/login")
+        }
+    })
+})
+
+app.get("/secrets", (req, res) => {
+    User.find({ "secret": { $ne: null } }, (err, foundUsers) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                if (req.isAuthenticated()) {
+                    res.render("secrets", { usersWithSecrets: foundUsers })
+                } else {
+                    res.redirect("/login")
+                }
+            }
+        }
+    })
+});
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login")
+    }
+})
+
+
+// POST REQUESTS
+app.post("/submit", (req, res) => {
+
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user._id, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(() => {
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
+
+})
+
+app.post("/register", (req, res) => {
+
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets")
+            })
+        }
+    })
+
+});
+
+app.post("/login", (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, (err) => {
+        if (!user) {
+            res.render("No such user found")
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            })
+        }
+    })
+
+})
+
+app.use(notFoundMiddleware)
+
+app.listen(3000, () => {
+    console.log("Server is up and running on port 3000");
+})
